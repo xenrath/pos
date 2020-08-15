3d4418886015ba33561ac29637ba897cfbec0710
xenrath <xenrath89@gmail.com>
2020-08-15 11:57:48 +0700

    pos

---
+++ application/views/dashboard.php
@@ -0,0 +1,56 @@
+<section class="content-header">
+  <h1> Dashboard
+    <small>Control Panel</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+    <li class="active">Dashboard</li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <div class="row">
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-aqua"><i class="fa fa-th"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Items</span>
+          <span class="info-box-number">90</span>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-red"><i class="fa fa-truck"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Supplier</span>
+          <span class="info-box-number">4</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="clearfix visible-sm-block"></div>
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-green"><i class="fa fa-users"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Customers</span>
+          <span class="info-box-number">60</span>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-yellow"><i class="fa fa-user-plus"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Users</span>
+          <span class="info-box-number">3</span>
+        </div>
+      </div>
+    </div>        
+  </div>
+
+</section>
---
+++ application/views/product/category/category_data.php
@@ -0,0 +1,55 @@
+<section class="content-header">
+  <h1> Categories
+    <small>Kategori Produk</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+    <li class="active">Categories</li>
+  </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+	<div class="box">
+		<div class="box-header">
+			<h3 class="box-title">Data Categories</h3>
+			<div class="pull-right">
+				<a href="<?=site_url('category/add') ?>" class="btn btn-primary btn-flat">
+					<i class="fa fa-user-plus"></i> Create
+				</a>
+			</div>
+		</div>
+		<div class="box-body table-responsive">
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$no = 1;
+					foreach ($row->result() as $key => $data) {
+					 ?>
+					<tr>
+						<td><?=$no++?>.</td>
+						<td><?=$data->name?></td>
+						<td class="text-center" width="160px">
+							<a href="<?=site_url('category/edit/'.$data->category_id) ?>" class="btn btn-primary btn-xs">
+								<i class="fa fa-pencil"></i> Edit
+							</a>
+							<a href="<?=site_url('category/del/'.$data->category_id) ?>" onclick="return confirm('Hapus Data?')" class="btn btn-danger btn-xs">
+								<i class="fa fa-trash"></i> Delete
+							</a>
+						</td>
+					</tr>
+				<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	
+</section>
