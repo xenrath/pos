429fd35a533b480ae6cedf1fc9cb864130d34f20
xenrath <xenrath89@gmail.com>
2020-08-20 22:34:28 +0700

    Hapus Data Sekaligus Gambar dan Custom Messages [28]

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -127,9 +127,10 @@ class Item extends CI_Controller {
 						redirect('item');
 
 					}else{
+						$item = $this->item_m->get($post['id'])->row();
 						$error = $this->upload->display_errors();
 						$this->session->set_flashdata('error', $error);
-						redirect('item/edit');
+						redirect('item/edit/'.$post['id']);
 					}
 				}else{
 					$post['image'] = null;
@@ -145,6 +146,11 @@ class Item extends CI_Controller {
 
 	public function del($id)
 	{
+		$item = $this->item_m->get($id)->row();
+		if ($item->image != null) {
+			$target_file = './uploads/product/'.$item->image;
+			unlink($target_file);
+		}
 		$this->item_m->del($id);
 		if ($this->db->affected_rows() > 0) {
 			$this->session->set_flashdata('success', 'Data berhasil dihapus');
