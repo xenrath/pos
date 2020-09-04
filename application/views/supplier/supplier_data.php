3978a73b4e4e28ec5cd9289a5400fd33a0893f69
xenrath <xenrath89@gmail.com>
2020-09-04 19:39:37 +0700

    Alert Konfirmasi Elegan dengan Modal Bootstrap [43]

--- application/views/supplier/supplier_data.php
+++ application/views/supplier/supplier_data.php
@@ -47,7 +47,10 @@
 							<a href="<?=site_url('supplier/edit/'.$data->supplier_id) ?>" class="btn btn-primary btn-xs">
 								<i class="fa fa-pencil"></i> Edit
 							</a>
-							<a href="<?=site_url('supplier/del/'.$data->supplier_id) ?>" onclick="return confirm('Hapus Data?')" class="btn btn-danger btn-xs">
+							<!-- <a href="<?=site_url('supplier/del/'.$data->supplier_id) ?>" onclick="return confirm('Hapus Data?')" class="btn btn-danger btn-xs">
+								<i class="fa fa-trash"></i> Delete
+							</a> -->
+							<a href="#modal-delete" data-toggle="modal" onclick="$('#modal-delete #formDelete').attr('action', '<?=site_url('supplier/del/'.$data->supplier_id) ?>')" class="btn btn-danger btn-xs">
 								<i class="fa fa-trash"></i> Delete
 							</a>
 						</td>
@@ -59,3 +62,22 @@
 	</div>
 
 </section>
+
+<div class="modal fade" id="modal-delete">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title">Yakin menghapus data ini?</h4>
+			</div>
+			<div class="modal-footer">
+				<form id="formDelete" action="" method="post">
+					<button class="btn btn-default" data-dismiss="modal">Tidak</button>
+					<button class="btn btn-danger" type="submit">Hapus</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
